c93c9551192087f06d50542756e04fa564d70ab3
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-08-06 19:51:41 +0530

    added roberta android

---
+++ roberta_android.py
@@ -0,0 +1,33 @@
+import torch
+from transformers import AutoModelForMaskedLM
+from executorch.backends.xnnpack.partition.xnnpack_partitioner import XnnpackPartitioner
+from executorch.exir import to_edge_transform_and_lower
+from torch.export import export, Dim
+
+
+model = AutoModelForMaskedLM.from_pretrained(
+    "FacebookAI/roberta-base", torch_dtype=torch.float16
+).eval()
+
+
+SEQ_LEN = 128
+dummy_ids = torch.zeros((1, SEQ_LEN), dtype=torch.long)
+dummy_attn = torch.ones_like(dummy_ids)
+
+example_inputs = (dummy_ids, dummy_attn)
+
+dynamic_shapes = {
+    "input_ids": {1: Dim("seq", min=1, max=512)},
+    "attention_mask": {1: Dim("seq", min=1, max=512)},
+}
+
+
+exported = export(model, example_inputs, dynamic_shapes=dynamic_shapes)
+pte_program = to_edge_transform_and_lower(
+    exported, partitioner=[XnnpackPartitioner()]
+).to_executorch()
+
+with open("roberta-base-fillmask.pte", "wb") as f:
+    f.write(pte_program.buffer)
+
+print("roberta-base-fillmask.pte created")
